7c33dea5784b22180d59292fe56171efb9ea0731
Zak <zspratt21@gmail.com>
2024-01-30 16:44:36 +0800

    Save user on register so scout index triggers.

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -44,7 +44,7 @@ class RegisteredUserController extends Controller
         ]);
 
         event(new Registered($user));
-
+        $user->save();
         Auth::login($user);
 
         return redirect(RouteServiceProvider::HOME);
