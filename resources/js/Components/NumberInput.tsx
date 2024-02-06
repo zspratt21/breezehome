82989ecccb011cd8957db6941653ee9a69db98ae
Zak <zspratt21@gmail.com>
2024-02-06 13:10:50 +0800

    fixed small issues with number input & added text area input.

--- resources/js/Components/NumberInput.tsx
+++ resources/js/Components/NumberInput.tsx
@@ -1,12 +1,12 @@
 import TextInput from './TextInput';
 import { InputHTMLAttributes } from 'react';
 
-export default function NumberInput(props: InputHTMLAttributes<HTMLInputElement>) {
+export default function NumberInput(props: InputHTMLAttributes<HTMLInputElement> & { isFocused?: boolean }) {
     return (
         <TextInput
             {...props}
             onKeyDown={(e) => {
-                if (!/[0-9]/.test(e.key) && e.key !== 'Backspace' && e.key !== 'Enter') {
+                if (!/[0-9]/.test(e.key) && e.key !== 'Backspace' && e.key !== 'Enter' && e.key !== 'Tab') {
                     e.preventDefault();
                 }
             }}
