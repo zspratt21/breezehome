bebcfea6208d11e5a5481b9c87195f1075ad7ae9
Zak <zspratt21@gmail.com>
2024-07-09 13:49:19 +0800

    Updated to Laravel 11. Updated Docker environment containers to latest versions. Fixed typing in GuestLayout.

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren, ReactNode } from 'react';
 import NavLink from '@/Components/NavLink';
 import MainLayout from './MainLayout';
 
-export default function Guest({ children, header }: PropsWithChildren<{header: ReactNode}>) {
+export default function Guest({ children, header = null }: PropsWithChildren<{header?: ReactNode}>) {
     const navigationLinks = (
         <>
             <NavLink href="/login" active={route().current('login')}>Login</NavLink>
