e2ff811016fcde22e8cab723863dbdf9b2a358ea
Zak <zspratt21@gmail.com>
2024-07-25 09:19:40 +0800

    added cli container for running tests and various scripts.
    added preparation method to profile update request.
    
    added seperate testing env for testing.
    
    added command scripts for linting and testing.

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -9,6 +9,16 @@ use Illuminate\Validation\Rule;
 class ProfileUpdateRequest extends FormRequest
 {
     /**
+     * Prepare the data for validation.
+     */
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'remove_avatar' => $this->input('remove_avatar', 0),
+        ]);
+    }
+
+    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
