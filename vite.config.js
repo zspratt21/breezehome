65af984057fed643666d62d90691032958c7fd67
Zak <zspratt21@gmail.com>
2024-08-20 16:28:57 +0800

    Set host to vite pusher host.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import fs from 'fs';
 
 const port = process.env.VITE_PORT || 5173;
-const appHost = process.env.APP_HOST || 'localhost';
+const appHost = process.env.VITE_PUSHER_HOST || 'localhost';
 const sslCertPath = process.env.SSL_CERTIFICATE_PATH || './docker/ssl/certs/localhost.crt';
 const sslKeyPath = process.env.SSL_CERTIFICATE_KEY_PATH || './docker/ssl/certs/localhost.key';
 export default defineConfig({
